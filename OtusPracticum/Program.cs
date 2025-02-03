1d80526efe42236fc8a3883c469463fe00d7d826
Andreev D.A. <d.andreev@abonentplus.ru>
2025-02-03 09:51:35 +0300

    Добавьте файлы проекта.

---
+++ OtusPracticum/Program.cs
@@ -0,0 +1,15 @@
+namespace OtusPracticum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
